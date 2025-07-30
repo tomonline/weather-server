8936ebf065d2a51548eea4cded0d5d79afc67688
tom <github@iamtom.online>
2025-07-30 10:01:15 +0100

    Updating default message

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.send("Sample Weather Server is running");
 });
 
 app.get("/cities", (req, res) => {
